9c441535be88446c0b734569c66db34967631826
Max Chaston <mdic1g21@soton.ac.uk>
2023-05-03 08:36:30 +0100

    Added question 6 solution

--- HASKELL SOLUTIONS.hs
+++ HASKELL SOLUTIONS.hs
@@ -10,7 +10,7 @@ combineTilesRight (Tile xs) (Tile ys) = Tile (combineLineArrays xs ys)
 
 combineLineArrays :: [String] -> [String] -> [String]
 combineLineArrays [] [] = []
-combineLineArrays _ [] = error "Not same tile size" 
+combineLineArrays _ [] = error "Not same tile size"
 combineLineArrays [] _ = error "Not same tile size"
 combineLineArrays (x:xs) (y:ys) = [x++y] ++ combineLineArrays xs ys
 
@@ -18,19 +18,19 @@ combineLineArrays (x:xs) (y:ys) = [x++y] ++ combineLineArrays xs ys
 --Used to duplicate a tile right for specified amount
 duplicateTileRight :: TileExp -> Int -> TileExp
 duplicateTileRight (tile) amount | amount == 1 = tile
-				 | otherwise = (combineTilesRight (tile) (duplicateTileRight tile (amount-1)))
+                                 | otherwise = (combineTilesRight (tile) (duplicateTileRight tile (amount-1)))
 
 
 
 --Used to duplicate a tile down for a specified amount
 duplicateTileDown :: TileExp -> Int -> TileExp
 duplicateTileDown (tile) amount | amount == 1 = tile
-				| otherwise = (combineTilesDown (tile) (duplicateTileDown tile (amount-1)))
+                                | otherwise = (combineTilesDown (tile) (duplicateTileDown tile (amount-1)))
 
 
 
 --Used to combine two tiles above/below each other
-combineTilesDown :: TileExp -> TileExp -> TileExp 
+combineTilesDown :: TileExp -> TileExp -> TileExp
 combineTilesDown (Tile xs) (Tile ys) = Tile (xs ++ ys)
 
 
@@ -43,7 +43,7 @@ rotateTile270Degrees (tile) = rotateTile90Degrees $ rotateTile90Degrees $ rotate
 
 rotateHelper :: [String] -> [String]
 rotateHelper (xs) | length (xs!!0) > 1 = getFirst xs : rotateHelper (map tail xs)
-		  | otherwise = [getFirst xs]
+                  | otherwise = [getFirst xs]
 
 getFirst :: [String] -> String
 getFirst [] = []
@@ -54,7 +54,7 @@ getFirst (x:xs) = getFirst xs ++ [head x]
 
 
 --Combines all 4 rotated tiles into one big tile
-squareRotateTile (x) = combineTilesDown (combineTilesRight x (rotateTile90Degrees x)) (combineTilesRight (rotateTile270Degrees x) (rotateTile180Degrees x)) 
+squareRotateTile (x) = combineTilesDown (combineTilesRight x (rotateTile90Degrees x)) (combineTilesRight (rotateTile270Degrees x) (rotateTile180Degrees x))
 
 
 
@@ -93,11 +93,11 @@ reflectTileXY (tile) = reflectTileY $ reflectTileX tile
 
 --Used to conjunct two tiles
 conjunctTiles (Tile x) (Tile y) = Tile (map zipTiles (zip x y))
-	where zipTiles (x,y) = zipWith (\x y -> if all (=='1') [x,y] then '1' else '0') x y
+        where zipTiles (x,y) = zipWith (\x y -> if all (=='1') [x,y] then '1' else '0') x y
 
 --Used to negate a tile
 negateTile (Tile x) = Tile (map negateRow x)
-	where negateRow = map (\x -> if x == '0' then '1' else '0')
+        where negateRow = map (\x -> if x == '0' then '1' else '0')
 
 
 --Used to create a sub list
@@ -119,30 +119,30 @@ prettyPrint (Tile xs) = putStrLn $ id $ intercalate "\n" xs
 
 --question1 (Tile ["1"]) (Tile ["0"])
 question1 (inp1) (inp2) = prettyPrint tile4
-	where tile1 = combineTilesRight inp1 inp2
-	      tile2 = duplicateTileRight tile1 32
-	      tile3 = combineTilesDown tile2 (rotateTile180Degrees tile2)
-	      tile4 = duplicateTileDown tile3 32
+        where tile1 = combineTilesRight inp1 inp2
+              tile2 = duplicateTileRight tile1 32
+              tile3 = combineTilesDown tile2 (rotateTile180Degrees tile2)
+              tile4 = duplicateTileDown tile3 32
 
 
 
 --question2 (Tile ["0001","0011","0111","1111"])
 question2 (inp) = prettyPrint $ tile5
-	where tile1 = squareRotateTile inp
+        where tile1 = squareRotateTile inp
               tile2 = combineTilesRight (tile1) (tile1)
               tile3 = combineTilesRight (tile2) (combineTilesDown inp (rotateTile270Degrees inp))
-	      tile4 = combineTilesDown (tile3) (combineTilesRight (combineTilesDown tile1 (combineTilesRight inp (rotateTile90Degrees inp))) (scaleTile inp 3))
+              tile4 = combineTilesDown (tile3) (combineTilesRight (combineTilesDown tile1 (combineTilesRight inp (rotateTile90Degrees inp))) (scaleTile inp 3))
               tile5 = squareRotateTile tile4
 
 --question3 (Tile ["000", "000", "011"])
 question3 (inp) = prettyPrint $ tile6
-	where tile1 = combineTilesRight (combineTilesRight inp (reflectTileY inp)) (createBlankTile inp)
-	      tile2 = combineTilesRight (combineTilesRight (createBlankTile inp) (reflectTileX inp)) (reflectTileXY inp)
-	      baseTile = combineTilesDown tile1 tile2
-	      tile3 = combineTilesRight (baseTile) (reflectTileX baseTile)
-	      tile4 = combineTilesDown (tile3) (tile3)
-	      tile5 = duplicateTileRight tile4 10
-	      tile6 = duplicateTileDown tile5 30
+        where tile1 = combineTilesRight (combineTilesRight inp (reflectTileY inp)) (createBlankTile inp)
+              tile2 = combineTilesRight (combineTilesRight (createBlankTile inp) (reflectTileX inp)) (reflectTileXY inp)
+              baseTile = combineTilesDown tile1 tile2
+              tile3 = combineTilesRight (baseTile) (reflectTileX baseTile)
+              tile4 = combineTilesDown (tile3) (tile3)
+              tile5 = duplicateTileRight tile4 10
+              tile6 = duplicateTileDown tile5 30
 
 --question4 (Tile ["11","00"]) (Tile ["00","11"]) (Tile ["01","10"])
 -- question4 :: TileExp -> TileExp -> TileExp -> [[Char]]
@@ -169,8 +169,23 @@ question5 (inp) = prettyPrint $ tile4
 -}
 
 
---question6 (Tile ["111","111","111"]) (Tile ["010","111"],"010") (Tile ["101,010,101"])
-question6 inp1 inp2 inp3 = prettyPrint $ inp1
+--question6 (Tile ["111","111","111"]) (Tile ["010","111","010"]) (Tile ["101","010","101"])
+question6 inp1 inp2 inp3 = prettyPrint $ foldl combineTilesDown (Tile [""]) (map (foldl combineTilesRight (createBlankTile inp1)) allLinesTiles)
+        where
+                blankTile = createBlankTile inp1
+                firstLine = [0 : 0 : take 58 (cycle [1,2,3])]
+                secondLine = [0 : take 59 (cycle [2,3,1])]
+                secondLastLine = [take 59 (cycle [1,2,3]) ++ [0]]
+                lastLine = [take 58 (cycle [3,1,2]) ++ [0,0]]
+                middleLines = map makeLine (take 56 (cycle [1,2,3]))
+                makeLine num
+                  | num==1 = take 60 (cycle[3,1,2])
+                  | num==2 = take 60 (cycle[2,3,1])
+                  | otherwise = take 60 (cycle[1,2,3])
+                allLines = firstLine ++ secondLine ++ middleLines ++ secondLastLine ++ lastLine
+                allLinesTiles = map (map (\x -> if x==1 then inp1 else if x==2 then inp2 else if x==3 then inp3 else blankTile)) allLines
+
+
 
 
 --question7 (Tile ["01","10"])
@@ -189,9 +204,9 @@ question9 inp1 = prettyPrint $ inp1
 
 --question10 (Tile ["00000000000000000000", "00000011111111000000", "00000111111111100000", "00001111111111110000", "00011111111111111000", "00111001111110011100", "00111001111110011100", "00111111111111111100", "01111111111111111110", "01111111111111111110", "01111111111111111110", "01111111111111111110", "00111111111111111100", "00111001111111001100", "00111100000000011100", "00011111111111111000", "00001111111111110000", "00000111111111100000", "00000011111111000000", "00000000000000000000"]) (Tile ["11111","10101","11011","10101","11111"])
 question10 inp1 inp2 = prettyPrint $ tile6
-	where tile1 = createSubTile inp1 0 0 999999 12
-	      tile2 = createSubTile inp1 0 12 12 5
-	      tile3 = createSubTile inp1 17 12 999999 5
-	      tile4 = createSubTile inp1 0 17 999999 999999
-	      tile5 = combineTilesRight (combineTilesRight tile2 inp2) tile3
-	      tile6 = combineTilesDown (combineTilesDown(tile1) (tile5)) tile4
+        where tile1 = createSubTile inp1 0 0 999999 12
+              tile2 = createSubTile inp1 0 12 12 5
+              tile3 = createSubTile inp1 17 12 999999 5
+              tile4 = createSubTile inp1 0 17 999999 999999
+              tile5 = combineTilesRight (combineTilesRight tile2 inp2) tile3
+              tile6 = combineTilesDown (combineTilesDown(tile1) (tile5)) tile4
